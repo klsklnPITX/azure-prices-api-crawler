a78a6ea0701544430d5d23828923a90c72f943d7
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2021-02-23 13:25:35 +0100

    testing

--- azure_prices_crawler.py
+++ azure_prices_crawler.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 
 BASE_URL = "https://prices.azure.com/api/retail/prices?$filter=location eq"
@@ -10,7 +11,7 @@ FIELDS = [
     "unitPrice",
     "location",
     "meterId",
-    "meterName,"
+    "meterName",
     "productId",
     "productName",
     "skuName",
@@ -29,14 +30,68 @@ ToDo:
 """
 
 
-def call_api(location):
+def csv_writer(data, filename):
+    with open(f'{filename}.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Write CSV headers
+        writer.writerow(FIELDS)
+
+        for value in data["Items"]:
+            writer.writerow([
+                value[FIELDS[0]],
+                value[FIELDS[1]],
+                value[FIELDS[2]],
+                value[FIELDS[3]],
+                value[FIELDS[4]],
+                value[FIELDS[5]],
+                value[FIELDS[6]],
+                value[FIELDS[7]],
+                value[FIELDS[8]],
+                value[FIELDS[9]],
+                value[FIELDS[10]],
+                value[FIELDS[1]],
+                value[FIELDS[12]],
+                value[FIELDS[13]],
+            ])
+
+
+def get_next_page(json_data):
+    if json_data["NextPageLink"]:
+        return json_data["NextPageLink"]
+    return False
+
+
+def call_next_page(current_page_data):
+    if not get_next_page(current_page_data):
+        return False
+
+    next_page_url = get_next_page(current_page_data)
+    r = requests.get(next_page_url)
+    new_data = json.loads(json.dumps(r.json()))
+    return new_data
+
+
+def get_api_data(location):
     r = requests.get(BASE_URL+f" '{location}'")
     data = json.loads(json.dumps(r.json()))
+    print("page 1")
+    if get_next_page(data):
+        next_page_exists = True
+        new_data = call_next_page(data)
+        data.update(new_data)
+        print("page 2")
 
-    # Get next page
-    if data["NextPageLink"]:
-        print(data["NextPageLink"])
-    # print(data)
+    while next_page_exists:
+        new_data = call_next_page(new_data)
+        if new_data:
+            data.update(new_data)
+            print(get_next_page(new_data))
+        else:
+            next_page_exists = False
+            print("done")
+
+    return data
 
 
-call_api("EU West")
+data = get_api_data("EU West")
+csv_writer(data, "eu-west-prices")
